68eb58219c27d60161e6052f878a444ce5c56e98
nagarjun523 <nagarjun@pixehub.com>
2020-02-18 12:54:09 +0530

    Remove User Modified
    Remove User Modified

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -1,5 +1,5 @@
-
 import * as React from "react";
+
 
 interface IProps {
     user? : string;
@@ -12,6 +12,9 @@ interface IState {
     pickedUsers?: any;
     username?: any;
     submitted?:any;
+    errMsg? : string;
+    emptyUserList? :boolean;
+    pickedUserEmpty? : boolean;
    
 }
 export class Users extends React.Component<IProps, IState> 
@@ -21,11 +24,16 @@ export class Users extends React.Component<IProps, IState>
         this.handleUserNameChange = this.handleUserNameChange.bind(this);
         this.addUser = this.addUser.bind(this);
         this.removeUser = this.removeUser.bind(this);
+        this.pickedUser = this.pickedUser.bind(this);
         this.state = {
             newUser : '',
             pickedUser: '',
             users : [],
-            pickedUsers: []
+            pickedUsers: [],
+            errMsg : 'This value is required.',
+            emptyUserList : true,
+            pickedUserEmpty : true
+            
         };
       }
    addUser(){
@@ -37,58 +45,57 @@ export class Users extends React.Component<IProps, IState>
             });
        this.setState({
         users : users,
-        newUser : ''
+        newUser : '',
+        emptyUserList : false
        })
     }   
    };
    removeUser(_value: React.ReactNode){
-    var array = [...this.state.users]; // make a separate copy of the array
-    var index = array.indexOf(_value)
-    // if (index == -1) {
-      array.splice(index, 1);
-      this.setState({users: array});
-    // }
+    var list = [];
+    var array = this.state.users; // make a separate copy of the array
+    var arrayLenth =  array.length;
+    for(var k = 0; k< arrayLenth; k++){
+      list.push(array[k]['username']);
+    }
+    var index = list.indexOf(_value);
+    array.splice(index, 1);
+    this.setState({users: array});
     var pickedArray = [...this.state.pickedUsers]; // make a separate copy of the array
     var pickedIndex = pickedArray.indexOf(_value)
-    // if (index == -1) {
     pickedArray.splice(pickedIndex, 1);
     this.setState({pickedUsers: pickedArray});
    };
-   pickedUser(_value: React.ReactNode){     
-     var users = [...this.state.users];
+   pickedUser(e:React.ReactNode){     
+     var users = this.state.users;
      var usersLength = users.length;
      // if(usersLength != 0){
-       var pickedUser = users[Math.floor(Math.random()*users.length)];
-       var userVal = pickedUser.username;
-       var pickedUsers = [...this.state.pickedUsers];
-       var userLength = pickedUsers.length;
-       if(userLength == 0){
-        pickedUsers.push({
-             'username' : userVal
-            });
-            this.setState({
-                pickedUsers : pickedUsers,
-                pickedUser : ''
-           })
-        }else{
-           for(var j = 0; j < userLength; j++){           
-             if(pickedUsers[j]['username'] != userVal){
-                 pickedUsers.push({
-                 'username' : userVal
-                });
-                this.setState({
-                    pickedUsers : pickedUsers,
-                    pickedUser : ''
-               })
-             }else{
-               return;
-             }
-        }
-      }     
+        //  this.setState({
+        //      pickedUsers : []
+        //  })
+        //var pickedUsers = [...this.state.pickedUsers];
+    if(!this.state.pickedUser){
+        var pickedUsers = [...this.state.users];
+    }else{
+        var pickedUsers = [...this.state.pickedUsers];
+       
+    }
+    if(pickedUsers.length !=0){
+        var pickedUser = pickedUsers[Math.floor(Math.random() * pickedUsers.length)];
+        var userVal = pickedUsers.splice(pickedUser,1);
+        this.setState({
+            pickedUserEmpty : false,
+            pickedUsers : pickedUsers,
+            pickedUser: userVal[0].username
+        })  
+    }else{
+
+    }
+  
    };
    handleUserNameChange (e: React.ChangeEvent<HTMLInputElement>){
         this.setState({
-            newUser : e.target.value
+            newUser : e.target.value,
+            errMsg : ''
         });
 
    }
@@ -96,10 +103,11 @@ export class Users extends React.Component<IProps, IState>
     {
       const { username,submitted} = this.state;
         return (
-            <div className="container">
+            <div className="container bg-light">
                 <br></br>
+                <h3 className="mb-5 text-primary">React-Typescript-Webpack Frontend Test Task</h3>
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-6">
                 <div className="card">
                     <div className="card-header">
                     <h5>Add User</h5>
@@ -110,7 +118,7 @@ export class Users extends React.Component<IProps, IState>
                                 <label>Username</label>
                                 <input type="text" name="username" value={this.state.newUser} onChange={this.handleUserNameChange} className="form-control" id="username" aria-describedby="eusernameHelp" />
                                 {submitted && !username &&
-                          <div className="help-block">This value is required.</div>}
+                                <div className="help-block text-danger">{this.state.errMsg}</div>}
                            </div>
                             <button type="button" onClick={this.addUser} className="btn btn-md btn-primary">Add</button>
                     
@@ -118,40 +126,47 @@ export class Users extends React.Component<IProps, IState>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-6">
                 <div className="card">
                     <div className="card-header">
                     <h5>Users List</h5>
                     </div>
                     <div className="">
                     <ul className="list-group">
-                        {this.state.users.map((user: { username: React.ReactNode; }) => 
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                         <p onClick={() => this.pickedUser(user.username)}>{user.username}</p>
+                        {this.state.users.map((user: { username: React.ReactNode; }, i: React.ReactText) => 
+                        <li key={i} className="list-group-item d-flex justify-content-between align-items-center">
+                         <p >{user.username}</p>
                             <button className="btn btn-sm btn-primary" onClick={() => this.removeUser(user.username)}>Remove</button>
                             </li>
                             )}
+                            { this.state.emptyUserList ?
+                            (<span className="mx-2 mt-2 alert alert-danger"> Users not found. Please add it</span>) : 
+                            (<span></span>)
+                            }
                         </ul>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-1">
+                <div className="mt-5 mb-5 offset-md-4 col-md-5">
                
-                    <button className="btn btn-sm btn-primary" onClick={() => this.pickedUser(this.state.pickedUsers)}>Random</button>
+                    <button className="btn btn-lg btn-primary" onClick={this.pickedUser}>Click here to pick user randomly</button>
                 
                 </div>
-                <div className="col-md-3">
+                <div className="offset-md-3 col-md-6">
                 <div className="card">
                     <div className="card-header">
                     <h5>Picked User</h5>
                     </div>
                     <div className="">
                     <ul className="list-group">
-                        {this.state.pickedUsers.map((user: { username: React.ReactNode; }) => 
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                         {user.username}
-                            </li>
-                            )}
+                    
+                            
+                            { this.state.pickedUserEmpty ? 
+                         (<span className="mx-2 mt-2 alert alert-danger">Not picked any users. Please click on random button</span>) :
+                         ( <li className="list-group-item d-flex justify-content-between align-items-center">
+                         {this.state.pickedUser}
+                            </li>)    
+                        }
                         </ul>
                         </div>
                     </div>
